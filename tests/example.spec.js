274781712b16ed5f3f7982d3329e23c25c8c6a85
Gal Sesek <gal.sesek@endava.com>
2023-06-02 13:59:24 +0200

    Modified example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -34,7 +34,7 @@ test("Clicking on web elements", async ({page}) => {
 test("Working with inputs", async ({page}) => {
   await page.goto("https://zero.webappsecurity.com/index.html");
   await page.click("#signin_button");
-  
+
   await page.type("#user_login", "some username");
   await page.type("#user_password", "some password");
   await page.click("text=Sign in");
@@ -42,3 +42,21 @@ test("Working with inputs", async ({page}) => {
   const errorMessage = await page.locator(".alet-error");
   await expect(errorMessage).toContainText("Login and/or password are wrong.");
 });
+
+test("Assertions", async ({page}) => {
+  await page.goto("https://www.example.com");
+  await expect(page).toHaveURL("https://www.example.com");
+  await expect(page).toHaveTitle("Example Domain");
+
+  const element = await page.locator("h1");
+  await expect(element).toBeVisible();
+  await expect(element).toHaveText("Example Domain");
+  await expect(element).toHaveCount(1);
+
+  const nonExistingElement = await page.locator("h5");
+  await expect(nonExistingElement).not.toBeVisible();
+});
+
+
+
+
