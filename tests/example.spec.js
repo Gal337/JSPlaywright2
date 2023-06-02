4e97dce33f227ed510954f3af439879d84a8044b
Gal Sesek <gal.sesek@endava.com>
2023-06-02 14:50:44 +0200

    Basics in example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-/* test("Selectors", async ({page}) => {
+test.skip("Selectors", async ({page}) => {
   //text
   await page.click("text=value of selector");
   //CSS
@@ -13,7 +13,7 @@ const {test, expect} = require('@playwright/test');
   await page.click("#username .first");
   //Xpath
   await page.click("//button");
-}); */
+});
 
 test("Simple basic test", async ({page}) => {
   await page.goto("https://www.example.com");
@@ -57,6 +57,35 @@ test("Assertions", async ({page}) => {
   await expect(nonExistingElement).not.toBeVisible();
 });
 
+//Putting tests into test suite
+test.describe("Test suite example", () => {
+  test("Working with inputs", async ({page}) => {
+    await page.goto("https://zero.webappsecurity.com/index.html");
+    await page.click("#signin_button");
+  
+    await page.type("#user_login", "some username");
+    await page.type("#user_password", "some password");
+    await page.click("text=Sign in");
+  
+    const errorMessage = await page.locator(".alet-error");
+    await expect(errorMessage).toContainText("Login and/or password are wrong.");
+  });
+  
+  test("Assertions", async ({page}) => {
+    await page.goto("https://www.example.com");
+    await expect(page).toHaveURL("https://www.example.com");
+    await expect(page).toHaveTitle("Example Domain");
+  
+    const element = await page.locator("h1");
+    await expect(element).toBeVisible();
+    await expect(element).toHaveText("Example Domain");
+    await expect(element).toHaveCount(1);
+  
+    const nonExistingElement = await page.locator("h5");
+    await expect(nonExistingElement).not.toBeVisible();
+  });
+  
+});
 
 
 
