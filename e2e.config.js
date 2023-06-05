f19f1ea604b844c4f9912bbe3030ee9a5491335b
Gal Sesek <gal.sesek@endava.com>
2023-06-05 17:23:41 +0200

    Modified E2E folder and file

---
+++ e2e.config.js
@@ -0,0 +1,30 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: 'tests/E2E',
+  timeout: 60000,
+  retries: 0,
+  use: {
+    headless: true,
+    viewport: {width: 1280, height: 720},
+    actionTimeout: 15000,
+    ignoreHTTPSErrors: true,
+    video: "off",
+    screenshot: "only-on-failure",
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium'},
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox'},
+    },
+    {
+      name: 'Webkit',
+      use: { browserName: 'webkit'},
+    }
+  ]
+
+});
