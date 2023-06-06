2b9af631e53f0dba7f0bb8ecbf92f50454970136
Gal Sesek <gal.sesek@endava.com>
2023-06-06 15:07:52 +0200

    Modified e2e-submit-form

--- tests/E2E/e2e-submit-form.spec.js
+++ tests/E2E/e2e-submit-form.spec.js
@@ -19,5 +19,12 @@ test.describe("Feedback Form", () => {
     await expect(commentInput).toBeEmpty();
   });
   //Submit Feedback form
-
+  test("Submit feedback form", async ({page}) => {
+    await page.type("#name", "some name");
+    await page.type("#email", "4testing@gmail.com");
+    await page.type("#subject", "some subject");
+    await page.type("#comment", "some nice comment about the application");
+    await page.click("input[type='submit']");
+    await page.waitForSelector("#feedback-title");
+  });
 });
