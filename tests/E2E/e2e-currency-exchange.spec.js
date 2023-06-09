343107f44129ada581f7ed843bdd312e93bede94
Gal Sesek <gal.sesek@endava.com>
2023-06-09 15:54:48 +0200

    Modified e2e-currency-exchange file

--- tests/E2E/e2e-currency-exchange.spec.js
+++ tests/E2E/e2e-currency-exchange.spec.js
@@ -14,21 +14,22 @@ test.describe.only("Currency Exchange", () => {
   test("Purchasing foreign currency", async ({page}) => {
     await page.click("#pay_bills_link");
     await page.click("text=Purchase Foreign Currency");
-    await page.selectOption("#pc_currency","Hong Kong (dollar)");
+    await page.selectOption("#pc_currency","EUR");
 
     const checkRate = await page.locator("#sp_sell_rate");
-    await expect(checkRate).toBeVisible();
+    await expect(checkRate).toContainText("1 euro (EUR)");
 
     await page.type("#pc_amount", "3021");
     await page.click("#pc_inDollars_true");
     await page.click("#pc_calculate_costs");
 
     const conversionAmount = await page.locator("#pc_conversion_amount");
-    await expect(conversionAmount).toBeVisible();
+    await expect(conversionAmount).toContainText("U.S. dollar (USD)");
 
     await page.click("#purchase_cash");
 
     const confirmExchange = await page.locator("#alert_content");
+    await expect(confirmExchange).toBeVisible();
     await expect(confirmExchange).toContainText("Foreign currency cash was successfully purchased.");
   });
 });
