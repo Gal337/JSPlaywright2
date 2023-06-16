aad605f22eddb99b3e8e55f2901283d0a7220681
Gal Sesek <gal.sesek@endava.com>
2023-06-16 11:32:58 +0200

    Modified visual.spec.js file

--- tests/visual/visual.spec.js
+++ tests/visual/visual.spec.js
@@ -6,4 +6,10 @@ test.describe.only("Visual Regression Testing example", () => {
     expect(await page.screenshot()).toMatchSnapshot("homepage.png");
   });
 
+  test("Single Element Snapshot", async ({page}) => {
+    await page.goto("https://www.example.com");
+    const pageElement = await page.$("h1");
+    expect(await pageElement.screenshot()).toMatchSnapshot("page-title.png");
+  });
+
 })
