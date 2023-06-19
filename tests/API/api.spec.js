a72623e403d4708935e1e30c094698ce08e1e520
Gal Sesek <gal.sesek@endava.com>
2023-06-19 15:51:10 +0200

    Modified api test file - added POST request tests

--- tests/API/api.spec.js
+++ tests/API/api.spec.js
@@ -29,4 +29,40 @@ test.describe.parallel("API Testing", () => {
     console.log(responseBody);
   });
 
+  test("POST Request - Create New User", async ({request}) => {
+    const response = await request.post(`${baseUrl}users`, { 
+    data: {
+      id: 1000,
+      }
+    });
+
+    const responseBody = JSON.parse(await response.text());
+    expect(responseBody.id).toBe(1000);
+    expect(responseBody.createdAt).toBeTruthy();
+  });
+
+  test("POST Request - Login", async ({request}) => {
+    const response = await request.post(`${baseUrl}login`, {
+      data: {
+        email: "eve.holt@reqres.in",
+        password: "cityslicka",
+      }
+    });
+
+    const responseBody = JSON.parse(await response.text());
+    expect(response.status()).toBe(200);
+    expect(responseBody.token).toBeTruthy();
+  });
+
+  test("POST Request - Failed Login", async ({request}) => {
+    const response = await request.post(`${baseUrl}login`, {
+      data: {
+        email: "peter@klaven",
+      }
+    });
+
+    const responseBody = JSON.parse(await response.text());
+    expect(response.status()).toBe(400);
+    expect(responseBody.error).toBe("Missing password");
+  });
 });
