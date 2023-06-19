11897d132ecceb2da278426ebc8ae625ef1b5786
Gal Sesek <gal.sesek@endava.com>
2023-06-19 14:48:20 +0200

    Modified api test file

--- tests/API/api.spec.js
+++ tests/API/api.spec.js
@@ -4,8 +4,11 @@ test.describe.parallel("API Testing", () => {
   const baseUrl = "https://www.reqres.in/api/";
 
   test("Simple API Test - Assert Response Status", async ({request}) => {
-    const response = await request.get(`${baseUrl}users/2`);
+    const response = await request.get(`${baseUrl}users/3`);
     expect(response.status()).toBe(404);
+
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
   });
 
   test("Simple API Test - Assert Invalid Endpoint", async ({request}) => {
