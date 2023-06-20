bedea3ebf5d5f0115ddb660df3ae19e2cd4f424b
Gal Sesek <gal.sesek@endava.com>
2023-06-20 14:30:40 +0200

    Added test that cycles through array to tips file

--- tests/Tips/tips.spec.js
+++ tests/Tips/tips.spec.js
@@ -10,4 +10,19 @@ test.describe.only("Tips & Tricks Section", () => {
     test.skip(browserName === "chromium", "Feature not ready in Chrome browser");
     await page.goto("http://zero.webappsecurity.com/");
   });
+
+  test("Test FixMe Annotation", async ({page, browserName}) => {
+    test.fixme(browserName === "chromium", "Test is not stable, needs revision");
+    await page.goto("http://zero.webappsecurity.com/");
+  });
+
+  const people = ["Mike", "Judy", "Elon", "Galadriel"];
+  for(const name of people) {
+    test.only(`Running test for ${name}`, async ({page}) => {
+      await page.goto("http://zero.webappsecurity.com/index.html");
+      await page.type("#searchTerm", `${name}`);
+      await page.waitForTimeout(3000);
+    });
+  }
+  
 })
