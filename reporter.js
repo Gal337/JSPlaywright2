7be11a869ae4a47b4139cadafcf2cada3c76e224
Gal Sesek <gal.sesek@endava.com>
2023-06-20 16:04:58 +0200

    Added reporter file (not working)

---
+++ reporter.js
@@ -0,0 +1,34 @@
+const { Reporter } = require('@playwright/test/reporter')
+const fs = require('fs');
+
+class CustomReporter extends Reporter {
+  onBegin(config, suite) {
+    console.log(`Execution of ${suite.allTests().length} tests`);
+  }
+
+  onEnd(result) {
+    console.log(`Execution finished with status of ${result.status}`);
+  }
+
+  onTestBegin(test) {
+    console.log(`Execution of ${test.title} started.`);
+  }
+
+  onTestEnd(test, result) {
+    const execTime = result.duration;
+    const data = {
+      test: test.title,
+      status: result.status,
+      executionTime: execTime,
+      errors: result.errors,
+    };
+
+    const dataToString = JSON.stringify(data, null, 2);
+    console.log(dataToString);
+
+    fs.writeFileSync("test-result.json", dataToString);
+  }
+
+}
+
+export default CustomReporter;
