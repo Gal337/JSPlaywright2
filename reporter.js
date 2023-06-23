1edf7c1a6ea47b439ea45e5414ba33d5aaacdb6f
Gal Sesek <gal.sesek@endava.com>
2023-06-23 16:13:12 +0200

    Created cucumber reporter for exporting reports

---
+++ reporter.js
@@ -0,0 +1,19 @@
+const reporter = require('cucumber-html-reporter');
+const options = { 
+  theme: 'bootstrap',
+  jsonFile: 'cucumber_report.json',
+  output: 'reports/cucumber_report.html',
+  reportSuiteAsScenario: true,
+  scenarioTimestamp: true,
+  launchReport: false,
+  metadata: {
+    'App version': '2.0.0',
+    'Test Environment': 'Staging',
+    Browser: 'Chrome 54.0',
+    Platform: 'Windows 10',
+    
+  }
+
+ }
+
+reporter.generate(options)
